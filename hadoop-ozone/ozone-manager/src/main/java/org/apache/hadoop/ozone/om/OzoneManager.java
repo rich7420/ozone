c589a9678bdac8c33998a0353fe4694204bfafbf
rich7420 <rc910420@gmail.com>
2025-11-10 15:52:26 +0800

    fix checkstyle test

--- hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/OzoneManager.java
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/OzoneManager.java
@@ -1045,14 +1045,14 @@ public final class OzoneManager extends ServiceRuntimeInfoImpl
       if (layoutVersionInDB != null) {
         if (versionManager.getMetadataLayoutVersion() < layoutVersionInDB) {
           LOG.info("New OM snapshot received with higher layout version {}. " +
-              "Attempting to finalize current OM to that version.",
-              layoutVersionInDB);
+                  "Attempting to finalize current OM to that version.",
+                  layoutVersionInDB);
           upgradeFinalizer.finalizeAndWaitForCompletion(
-              "om-ratis-snapshot", this,
-              config.getRatisBasedFinalizationTimeout());
+                  "om-ratis-snapshot", this,
+                  config.getRatisBasedFinalizationTimeout());
           if (versionManager.getMetadataLayoutVersion() < layoutVersionInDB) {
             throw new IOException("Unable to finalize OM to the desired layout " +
-                "version " + layoutVersionInDB + " present in the snapshot DB.");
+              "version " + layoutVersionInDB + " present in the snapshot DB.");
           } else {
             updateLayoutVersionInDB(versionManager, metadataManager);
           }
@@ -4325,7 +4325,7 @@ public final class OzoneManager extends ServiceRuntimeInfoImpl
     metadataManager.start(configuration);
     keyManager.start(configuration);
     try {
-    startSecretManagerIfNecessary();
+      startSecretManagerIfNecessary();
     } catch (Exception e) {
       throw new IOException("Failed to start secret manager", e);
     }
